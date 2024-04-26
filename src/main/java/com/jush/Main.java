4995ed9000bf24bc93ef4995412b633d136f51c5
Jushwanth <76085349+JushwanthGV@users.noreply.github.com>
2024-04-26 15:09:51 +0530

    complete discord bot code with lavaplayer included

--- src/main/java/com/jush/Main.java
+++ src/main/java/com/jush/Main.java
@@ -1,7 +1,48 @@
 package com.jush;
 
+import com.jush.commands.Ban;
+import com.jush.commands.Buttons;
+import com.jush.commands.Embed;
+import com.jush.commands.Modals;
+import com.jush.commands.Mute;
+import com.jush.commands.music.NowPlaying;
+import com.jush.commands.music.Play;
+import com.jush.commands.Staff;
+import com.jush.commands.Sum;
+import com.jush.commands.UnStaff;
+import com.jush.commands.Unmute;
+import com.jush.commands.music.Queue;
+import com.jush.commands.music.Repeat;
+import com.jush.commands.music.Skip;
+import com.jush.commands.music.Stop;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+
 public class Main {
-    public static void main(String[] args) {
-        JDA jda= JDABuilder.createDefault(token"").build;
+    static String discordToken = System.getProperty("discordToken");
+
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createDefault(discordToken).build();
+        jda.addEventListener(new Listeneres());
+        CommandManager manager = new CommandManager();
+        manager.add(new Sum());
+        manager.add(new Embed());
+        manager.add(new Buttons());
+        manager.add(new Modals());
+        manager.add(new Ban());
+        manager.add(new Mute());
+        manager.add(new Staff());
+        manager.add(new UnStaff());
+        manager.add(new Unmute());
+        manager.add(new Play());
+        manager.add(new Skip());
+        manager.add(new Stop());
+        manager.add(new NowPlaying());
+        manager.add(new Queue());
+        manager.add(new Repeat());
+        jda.addEventListener(manager);
     }
 }
